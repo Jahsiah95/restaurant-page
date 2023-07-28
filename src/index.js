be80037c85cb20b44012a4ef7fa34daece9b9eef
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-07-28 17:00:09 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const parent = function(parent){
     return parent
 };
 
-const addElement = function(id, type, parent, content){
+const addElement = function(id, group, type, parent, content){
 
     let name = id;
 
     name = document.createElement(type);
 
     name.setAttribute('id', id);
+    name.setAttribute('class', group)
     name.textContent = content;
     name.src = content;
 
@@ -21,40 +22,53 @@ const addElement = function(id, type, parent, content){
 }
 
 const index = (function(){
-    const logo = addElement('logo', 'div', parent('#container'), 'TonguePot');
-    const bio = addElement('bio', 'div', parent('#container'), 'Get that on ya tastebuds');
+    const logo = addElement('logo', '', 'div', parent('#container'), 'TonguePot');
+    const bio = addElement('bio', '', 'div', parent('#container'), 'Get that on ya tastebuds');
 
-    const tabs = addElement('tabs', 'ul', parent('#container'), '');
-    const home = addElement('home', 'li', parent('#tabs'), 'HOME');
-    const menu = addElement('menu', 'li', parent('#tabs'), 'MENU');
-    const contact = addElement('contact', 'li', parent('#tabs'), 'CONTACT');
+    const tabs = addElement('tabs', '', 'ul', parent('#container'), '');
+    const home = addElement('home', '','li', parent('#tabs'), 'HOME');
+    const menu = addElement('menu', '', 'li', parent('#tabs'), 'MENU');
+    const contact = addElement('contact', '', 'li', parent('#tabs'), 'CONTACT');
 
-    const hero = addElement('hero', 'div', parent('#container'), 'Welcome to TonguePot');
+    const hero = addElement('hero', '', 'div', parent('#container'), 'Welcome to TonguePot');
 
 })();
 
 const homePage = function(){
     parent('#hero').textContent = '';
     parent('#hero').textContent = 'Welcome to TonguePot'
-
 }
 
 const menuPage = function(){
     parent('#hero').textContent = '';
 
     let items = [
-    {type: 'Hambuger', price: '£8.99', ingredients: 'This, That, Etc.'},
-    {type: 'Chicken Burger', price: '£8.99', ingredients: 'This, That, Etc.'},
-    {type: 'Beef Burger', price: '£8.99', ingredients: 'This, That, Etc.'},
-    {type: 'Vegan Burger', price: '£8.99', ingredients: 'This, That, Etc.'},
-    {type: 'Fries', price: '£3.99', ingredients: 'This, That, Etc.'},
-    {type: 'Salad', price: '£3.99', ingredients: 'This, That, Etc.'}
-    ]
+    {type: 'Hambuger', ingredients: 'This, That, Etc.', price: '£8.99'},
+    {type: 'Chicken Burger', ingredients: 'This, That, Etc.', price: '£8.99'},
+    {type: 'Beef Burger', ingredients: 'This, That, Etc.', price: '£8.99'},
+    {type: 'Vegan Burger', ingredients: 'This, That, Etc.', price: '£8.99'},
+    {type: 'Fries', ingredients: 'This, That, Etc.', price: '£3.99'},
+    {type: 'Salad', ingredients: 'This, That, Etc.', price: '£3.99'}
+    ];
     console.log(items)
 
-    parent('#hero').textContent = items[0].type + ', ' + items[0].price
+    addElement('','heading','div',parent('#hero'),'DISH')
+    addElement('','heading','div',parent('#hero'),'INGREDIENTS')
+    addElement('','heading','div',parent('#hero'),'PRICE')
+    
+    items.forEach(item => {
+        for(let key in item){
+            if(item.hasOwnProperty(key)){
+                console.log(item[key])
+                addElement('','','div', parent('#hero'), `${item[key]}`)
+            };
+        };
+        // let key = Object.keys(item);
+        // let value = Object.values(item).join(' ');
+        // console.log(value);
+    });
 
-}
+};
 
 const contactPage = function(){
 
